6f293c85b2dff1fab460b3269d49846a6443b6fe
bermudatanker-spec <bermudatanker-spec@users.noreply.github.com>
2025-11-23 22:31:23 +0100

    Fix: pages

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+// pages/_document.tsx
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document.js";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
